3ab55f8c1daa3975b11420f315f813cdad4695a1
Lokesh Mohanty <lokesh1197@gmail.com>
2023-10-05 19:25:12 +0530

    add src files

---
+++ src/tests/model.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow.keras as keras
+
+model_filepath = "cifar10.keras"
+
+def test_model():
+    (_, _), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+    test_images = test_images.astype('float32') / 255
+
+    model = keras.models.load_model(model_filepath)
+    predictions = np.argmax(model.predict(test_images), axis=1)
+
+    assert predictions[0] == test_labels[0][0]
