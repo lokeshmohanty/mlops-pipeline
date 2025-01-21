2f4858b157c916426a5b3507e1b76385d86a011c
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2025-01-21 22:08:14 +0530

    add linting errors

--- src/run.py
+++ src/run.py
@@ -6,9 +6,9 @@ model = "data/cifar10.keras"
 labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 def predict(image, model):
-    image = np.array(
-        Image.open(image).resize((32, 32))
-    ).reshape(1, 32, 32, 3)
+    image = np.array(Image.open(image).resize((32, 32))).reshape(1, 32, 32, 3)
     model = keras.models.load_model(model)
     prediction = np.argmax(model.predict(image), axis=1)
     return labels[prediction[0]]
+
+
