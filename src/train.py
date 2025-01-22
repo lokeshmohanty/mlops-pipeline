38288dd70214b254530ff650ee1ae002337193e9
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2025-01-22 14:47:09 +0530

    fix lint errors

--- src/train.py
+++ src/train.py
@@ -10,8 +10,8 @@ model_filepath = "data/cifar10.keras"
 
 data = keras.datasets.cifar10.load_data()
 (train_images, train_labels), (test_images, test_labels) = data
-train_images = train_images.astype('float32') / 255
-test_images = test_images.astype('float32') / 255
+train_images = train_images.astype("float32") / 255
+test_images = test_images.astype("float32") / 255
 
 num_classes = 10
 train_labels = keras.utils.to_categorical(train_labels, num_classes)
@@ -19,36 +19,42 @@ test_labels = keras.utils.to_categorical(test_labels, num_classes)
 
 model = Sequential()
 
-model.add(layers.Conv2D(32, (3, 3),
-                        padding='same',
-                        activation='relu',
-                        input_shape=(32, 32, 3)))
+model.add(
+    layers.Conv2D(
+        32, (3, 3), padding="same", activation="relu", input_shape=(32, 32, 3)
+    )
+)
 model.add(layers.BatchNormalization())
-model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(layers.Conv2D(32, (3, 3), padding="same", activation="relu"))
 model.add(layers.BatchNormalization())
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(layers.Dropout(0.3))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation="relu"))
 model.add(layers.BatchNormalization())
 model.add(layers.Dropout(0.5))
-model.add(layers.Dense(num_classes, activation='softmax'))
+model.add(layers.Dense(num_classes, activation="softmax"))
 
 model.summary()
 
-model.compile(optimizer='adam',
+model.compile(optimizer="adam",
               loss=keras.losses.categorical_crossentropy,
-              metrics=['accuracy'])
+              metrics=["accuracy"])
 
 
 task = Task.init(
-    project_name = "CIFAR10",
-    task_name = "Train CNN",
-    auto_connect_frameworks = { "keras": model_filepath }
+    project_name="CIFAR10",
+    task_name="Train CNN",
+    auto_connect_frameworks={"keras": model_filepath},
 )
-history = model.fit(train_images, train_labels, batch_size=64, epochs=1,
-                    validation_data=(test_images, test_labels))
+history = model.fit(
+    train_images,
+    train_labels,
+    batch_size=64,
+    epochs=1,
+    validation_data=(test_images, test_labels),
+)
 
 print(test_labels[0])
 
--- tests/model.py
+++ tests/model.py
@@ -2,17 +2,19 @@ import numpy as np
 import tensorflow.keras as keras
 from src.run import predict
 
-model = "data/cifar10.keras"
-image = "data/peregrine_falcon.jpg"
+model_path = "data/cifar10.keras"
+image_path = "data/peregrine_falcon.jpg"
+
 
 def test_model():
     (train_images, train_labels), (_, _) = keras.datasets.cifar10.load_data()
-    train_images = train_images.astype('float32') / 255
+    train_images = train_images.astype("float32") / 255
 
-    model = keras.models.load_model(model)
+    model = keras.models.load_model(model_path)
     predictions = np.argmax(model.predict(train_images), axis=1)
 
     assert predictions[0] == train_labels[0][0]
 
+
 def test_api():
-    assert predict(image, model) == "truck"
+    assert predict(image_path, model_path) == "truck"
